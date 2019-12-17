2004838a27dc5425d11487780c264db9d3b4ae38
Lee <dleodlf1004@naver.com>
2019-12-17 09:14:58 +0900

    Up

--- Model_회화/Main.py
+++ Model_회화/Main.py
@@ -34,16 +34,14 @@ class MainWindow(QMainWindow):
         if show_text != self.temp:
             self.temp = show_text
             send_web_text = show_text.replace(' ', '%20')
-            self.web.setUrl(QtCore.QUrl(
-                "https://translate.google.co.kr/?hl=ko#view=home&op=translate&sl=en&tl=ko&text={}".format(
-                    send_web_text)))
+            self.web.setUrl(QtCore.QUrl("https://translate.google.co.kr/?hl=ko#view=home&op=translate&sl=en&tl=ko&text={}".format(send_web_text)))
 
     def resizeEvent(self, e):
         self.web.setGeometry(QtCore.QRect(5, 30, self.geometry().width() - 10, self.geometry().height() - 20))
         self.Read_contents.setGeometry(QtCore.QRect(5, 5, self.geometry().width() - 10, 20))
 
     def keyPressEvent(self, e):
-        if e.key() == 16777264:  # F1 키로 호출
+        if e.key() == 16777264: # F1 키로 호출
             self.serch_win = search_window()
 
 
@@ -68,15 +66,15 @@ class search_window(QMainWindow):
 
         self.search_input_box = QtWidgets.QLineEdit(self)  # 1단 Search input box
         self.search_input_box.setGeometry(QtCore.QRect(5, 5, self.geometry().width() - 10, 30))
-
+        
         # Tabel
         self.search_result_box = QtWidgets.QTableWidget(self)
-        self.search_result_box.setWordWrap(True)  # 자동으로 줄 바꿈
+        self.search_result_box.setWordWrap(True)    # 자동으로 줄 바꿈
         self.search_result_box.resizeRowsToContents()
         self.search_result_box.setGeometry(QtCore.QRect(5, 40, self.geometry().width() - 10,
                                                         self.geometry().height() - 50))
         self.search_result_box.setAlternatingRowColors(True)
-        self.search_result_box.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)  # 1개 선택 가능
+        self.search_result_box.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)    # 1개 선택 가능
         self.search_result_box.cellChanged.connect(self.db_update_due_to_changed_val)
         self.search_result_box.setColumnCount(3)
         self.search_result_box.setHorizontalHeaderLabels(["DB_row", "Korea", "English"])
@@ -85,7 +83,7 @@ class search_window(QMainWindow):
         self.search_result_box.setColumnWidth(0, 50)
         self.search_result_box.setColumnWidth(1, (self.search_result_box.geometry().width() - 50) / 2 - 10)
         self.search_result_box.setColumnWidth(2, (self.search_result_box.geometry().width() - 50) / 2 - 10)
-
+        
         self.show()
 
     def db_update_due_to_changed_val(self):
@@ -93,9 +91,9 @@ class search_window(QMainWindow):
         selected_cell = self.search_result_box.currentIndex()
         if selected_cell.column() != -1:
             # 업데이트된 변수 찾기
-            get_changed_db_index = self.search_result_box.item(selected_cell.row(), 0).text()  # get DB_row
-            get_changed_db_k = self.search_result_box.item(selected_cell.row(), 1).text()  # get DB_row
-            get_changed_db_e = self.search_result_box.item(selected_cell.row(), 2).text()  # get DB_row
+            get_changed_db_index = self.search_result_box.item(selected_cell.row(), 0).text()   # get DB_row
+            get_changed_db_k = self.search_result_box.item(selected_cell.row(), 1).text()       # get DB_row
+            get_changed_db_e = self.search_result_box.item(selected_cell.row(), 2).text()       # get DB_row
             # db 업데이트
             self.db.loc[int(get_changed_db_index)] = [str(get_changed_db_k), str(get_changed_db_e)]
             # 저장하려면 F3 누르기
@@ -135,7 +133,7 @@ class search_window(QMainWindow):
             # print(get_searched_db)                        # db 업데이트 확인용
             # print(len(get_searched_db))                   # db 업데이트 확인용
 
-            self.search_result_box.setRowCount(len(get_searched_db))  # 데이터가 몇개가 존재하는지 테이블 row 선정
+            self.search_result_box.setRowCount(len(get_searched_db))    # 데이터가 몇개가 존재하는지 테이블 row 선정
 
             # --- 찾은 데이터들을 표로 표기
             i = 0
@@ -157,8 +155,8 @@ class search_window(QMainWindow):
             print('No Search Value')
             mg = QMessageBox(text='No Search Value')
             mg.exec_()
-            self.search_result_box.clear()  # 데이터 지우고
-            self.search_result_box.setRowCount(0)  # 표 row 0 으로 초기화
+            self.search_result_box.clear()          # 데이터 지우고
+            self.search_result_box.setRowCount(0)   # 표 row 0 으로 초기화
         # 자동 줄 바꿈
         self.search_result_box.resizeRowsToContents()
 
@@ -190,36 +188,34 @@ class search_window(QMainWindow):
 
     def save_all_file(self):
         # 빈 변수 지우기
-        self.db.replace('', np.nan, inplace=True)  # '' 빈변수를 nan 값으로 처리
-        self.db.dropna(inplace=True)  # nan 값 날리기
-        self.db.reset_index(drop=True, inplace=True)  # 숫자 인텍스 초기화
+        self.db.replace('', np.nan, inplace=True)       # '' 빈변수를 nan 값으로 처리
+        self.db.dropna(inplace=True)                    # nan 값 날리기
+        self.db.reset_index(drop=True, inplace=True)    # 숫자 인텍스 초기화
         # db 저장하기
         self.db.to_pickle('db.pkl')
 
     def keyPressEvent(self, e):
         # print(e.key())
-        if e.key() == 16777220:  # 엔터 키로 내용 서치
+        if e.key() == 16777220: # 엔터 키로 내용 서치
             self.update_table_from_search_input()
-        elif e.key() == 16777266:  # F3 키으로 변경된 내용 저장
+        elif e.key() == 16777266: # F3 키으로 변경된 내용 저장
             # db 저장하기
             self.save_all_file()
-        elif e.key() == 16777267:  # F4 키로 새로운 열 생성
+        elif e.key() == 16777267: # F4 키로 새로운 열 생성
             # 열생성
             self.make_db()
-        elif e.key() == 16777268:  # F5 키로 모든 db 보기
+        elif e.key() == 16777268: # F5 키로 모든 db 보기
             self.show_all_Db()
 
     def resizeEvent(self, e):
-        self.search_input_box.setGeometry(QtCore.QRect(5, 5, self.geometry().width() - 10, 30))
-        self.search_result_box.setGeometry(
-            QtCore.QRect(5, 40, self.geometry().width() - 10, self.geometry().height() - 50))
+        self.search_input_box.setGeometry(QtCore.QRect(5, 5, self.geometry().width()-10, 30))
+        self.search_result_box.setGeometry(QtCore.QRect(5, 40, self.geometry().width()-10, self.geometry().height()-50))
         # Tabel 열 너비
         self.search_result_box.setColumnWidth(0, 50)
-        self.search_result_box.setColumnWidth(1, (self.search_result_box.geometry().width() - 50) / 2 - 10)
-        self.search_result_box.setColumnWidth(2, (self.search_result_box.geometry().width() - 50) / 2 - 10)
+        self.search_result_box.setColumnWidth(1, (self.search_result_box.geometry().width()-50)/2-10)
+        self.search_result_box.setColumnWidth(2, (self.search_result_box.geometry().width()-50)/2-10)
         # 자동 줄 바꿈
         self.search_result_box.resizeRowsToContents()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
